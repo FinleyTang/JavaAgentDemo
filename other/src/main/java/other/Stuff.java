23007f4dc49e9cc700c8af4dcc5f67021d2cde24
FinleyTang <finleyt@163.com>
2023-11-07 22:02:36 +0800

    premain在Java程序启动之前通过Java Agent机制向JVM注入代理代码的方式-     ->asm code 插桩

---
+++ other/src/main/java/other/Stuff.java
@@ -0,0 +1,10 @@
+package other;
+
+public class Stuff {
+    public void run() {
+        for(int i = 0; i <1000; i++){
+            System.out.println(i);
+        }
+        System.out.println("I AM DOING SOME STUFF...");
+    }
+}
